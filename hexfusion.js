958d14d26a0b8b315b83afc3209af0f25eeb8590
PMilzie <paulmiller_19@yahoo.com>
2010-04-11 01:52:28 -0400

    first commit

---
+++ hexfusion.js
@@ -0,0 +1,28 @@
+{
+    version: 3,
+    homepage: "http://forum.cyanogenmod.com/index.php?/topic/562-theme-hexfusion-cyanogenmod-updated-410/",
+    donate: "",
+    roms:
+    [
+    {
+        name: "HexFusion_CyanogenMod_v3.0.1",
+        summary: "HexFusion Theme for Droid CyanogenMod 5.0.5.7",
+        device: "sholes",
+        product: "HexFusionThemeSholes",
+        incremental: 301,
+        urls:
+        [
+        "http://github.com/downloads/PMilzie/HexFusion-Theme/HexFusion_CyanogenMod_v3.0.1-signed.zip",
+        ]
+    },
+    {
+        name: "HexFusion_CyanogenMod_NoIcons_v3.0.1",
+        summary: "HexFusion Theme for Droid CyanogenMod 5.0.5.7 without icons",
+        device: "sholes",
+        product: "HexFusionNoIconsSholes",
+        incremental: 301,
+        urls:
+        [
+        "http://github.com/downloads/PMilzie/HexFusion-Theme/HexFusion_CyanogenMod_NoIcons_v3.0.1-signed.zip",
+        ]
+}
