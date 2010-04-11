472da84e11255fc963d7e7287768b986a171f53b
PMilzie <paulmiller_19@yahoo.com>
2010-04-11 01:57:56 -0400

    first commit

---
+++ PMilzie.js
@@ -0,0 +1,10 @@
+{
+    developer: "PMilzie",
+    id: "PMilzie",
+    summary: "HexFusion Theme for Droid CyanogenMod",
+    manifest: "http://github.com/PMilzie/HexFusionTheme/blob/master/hexfusion.js",
+    roms:
+    {
+        sholes: true
+    },
+}
