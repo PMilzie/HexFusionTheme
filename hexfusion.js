009ea03c75a748565fb5de7543cea10bc43fe6d6
PMilzie <paulmiller_19@yahoo.com>
2010-04-14 22:17:31 -0400

    first commit

--- hexfusion.js
+++ hexfusion.js
@@ -1,10 +1,43 @@
 {
     version: 3,
     homepage: "http://forum.cyanogenmod.com/index.php?/topic/562-theme-hexfusion-cyanogenmod-updated-410/",
-    donate: "",
+    donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paulmiller_19%40yahoo%2ecom&lc=US&item_name=PMilzie%27s%20Themes&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted",
     roms:
     [
     {
+        name: "HexFusion_Theme_v3.0.3",
+        summary: "HexFusion Theme for Droid CyanogenMod 5.0.5.7",
+        device: "sholes",
+        product: "HexFusionSholes",
+        incremental: 303,
+        urls:
+        [
+        "http://github.com/downloads/PMilzie/HexFusionTheme/HexFusion_Complete_v3.0.3-signed.zip",
+        ]
+     }
+     {
+        name: "HexFusion_NoIcons_v3.0.3",
+        summary: "HexFusion Theme for Droid CyanogenMod 5.0.5.7 without icons",
+        device: "sholes",
+        product: "HexFusionNoIconsSholes",
+        incremental: 303,
+        urls:
+        [
+        "http://github.com/downloads/PMilzie/HexFusionTheme/HexFusion_NoIcons_v3.0.3-signed.zip",
+        ]
+     }
+     {
+        name: "HexToStock_v3.0.3",
+        summary: "Revert from either HexFusion theme back to stock theme",
+        device: "sholes",
+        product: "HexToStockSholes",
+        incremental: 303,
+        urls:
+        [
+        "http://github.com/downloads/PMilzie/HexFusionTheme/HexToStock_v3.0.3-signed.zip",
+        ]
+     }
+     {
         name: "HexFusion_NoIcons_v3.0.2",
         summary: "HexFusion Theme for Droid CyanogenMod 5.0.5.7 without icons",
         device: "sholes",
