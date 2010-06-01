0673fe3c5a5f81b02109b7c23e23a2a8ec9d3451
PMilzie <paulmiller_19@yahoo.com>
2010-06-01 01:36:34 -0400

    first commit

--- hexfusion.js
+++ hexfusion.js
@@ -1,62 +1,40 @@
 {
-    version: 3,
+    version: 4,
     homepage: "http://forum.cyanogenmod.com/index.php?/topic/562-theme-hexfusion-cyanogenmod-updated-410/",
     donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paulmiller_19%40yahoo%2ecom&lc=US&item_name=PMilzie%27s%20Themes&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted",
     roms:
     [
     {
-        name: "HexFusion_Theme_v3.0.7",
-        summary: "HexFusion Theme for Droid CyanogenMod 5.0.6.2",
+        name: "HexFusion_v4.0.0",
+        summary: "HexFusion Theme for Droid CyanogenMod 5.0.7.1",
         device: "sholes",
         product: "HexFusionSholes",
-        incremental: 307,
+        incremental: 400,
         urls:
         [
-        "http://github.com/downloads/PMilzie/HexFusionTheme/HexFusion_Complete_v3.0.7-signed.zip",
+        "http://github.com/downloads/PMilzie/HexFusionTheme/HexFusion_v4.0.0-signed.zip",
         ]
      },
      {
-        name: "HexFusion_NoIcons_v3.0.7",
-        summary: "HexFusion Theme for Droid CyanogenMod 5.0.6.2 without icons",
+        name: "HexLauncherPro_v4.0.0",
+        summary: "LauncherPro with HexFusion Theme, UNINSTALL launcher pro from market before installing this!",
         device: "sholes",
         product: "HexFusionNoIconsSholes",
-        incremental: 307,
+        incremental: 400,
         urls:
         [
-        "http://github.com/downloads/PMilzie/HexFusionTheme/HexFusion_NoIcons_v3.0.7-signed.zip",
+        "http://github.com/downloads/PMilzie/HexFusionTheme/HexLauncherPro_v4.0.0-signed.zip",
         ]
      },
      {
-        name: "Fusion_HelixLauncher2",
-        summary: "Themed Helix Launcher2, uninstall original helixlauncher2 before installing",
+        name: "Hex_HelixLauncher2",
+        summary: "HelixLauncher2 with HexFusion theme, UNINSTALL HelixLauncher2 from market before installing this!",
         device: "sholes",
-        product: "FusionHelixLauncher2Sholes",
-        incremental: 307,
+        product: "HexFusionNoIconsSholes",
+        incremental: 400,
         urls:
         [
         "http://github.com/downloads/PMilzie/HexFusionTheme/Hex_HelixLauncher2-signed.zip",
-        ]
-     },
-     {
-        name: "HexHome",
-        summary: "Themed original launcher",
-        device: "sholes",
-        product: "HexHomeSholes",
-        incremental: 307,
-        urls:
-        [
-        "http://github.com/downloads/PMilzie/HexFusionTheme/HexHome-signed.zip",
-        ]
-     },
-     {
-        name: "HexToStock_v3.0.7",
-        summary: "Revert from either HexFusion theme back to stock theme",
-        device: "sholes",
-        product: "HexToStockSholes",
-        incremental: 307,
-        urls:
-        [
-        "http://github.com/downloads/PMilzie/HexFusionTheme/HexToStock_v3.0.7-signed.zip",
         ]
      }
      ]
